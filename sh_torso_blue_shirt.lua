99b6de8872a42c5fefd2e633d0c70bddc2de0dc9
LordAerial <80987954+SculkStuff@users.noreply.github.com>
2023-07-30 22:26:12 +0200

    Add files via upload

---
+++ sh_torso_blue_shirt.lua
@@ -0,0 +1,36 @@
+ITEM.name = "Blue Shirt"
+ITEM.description = "A blue button down top. It's rather rough."
+ITEM.category = "Clothing"
+ITEM.model = "models/willardnetworks/clothingitems/torso_citizen1.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.iconCam = {
+    pos = Vector(-39.23, -7.19, 195.95),
+    ang = Angle(76.95, 10.07, 0),
+    fov = 4.05
+}
+ITEM.outfitCategory = "Torso"
+
+ITEM.bodyGroups = {
+    ["torso"] = 1
+}
+
+ITEM.allowedModels = {
+    "models/willardnetworks/citizens/female_01.mdl",
+    "models/willardnetworks/citizens/female_02.mdl",
+    "models/willardnetworks/citizens/female_03.mdl",
+    "models/willardnetworks/citizens/female_04.mdl",
+    "models/willardnetworks/citizens/female_06.mdl",
+    "models/willardnetworks/citizens/female_07.mdl",
+    
+    "models/willardnetworks/citizens/male_01.mdl",
+    "models/willardnetworks/citizens/male_02.mdl",
+    "models/willardnetworks/citizens/male_03.mdl",
+    "models/willardnetworks/citizens/male_04.mdl",
+    "models/willardnetworks/citizens/male_05.mdl",
+    "models/willardnetworks/citizens/male_06.mdl",
+    "models/willardnetworks/citizens/male_07.mdl",
+    "models/willardnetworks/citizens/male_08.mdl",
+    "models/willardnetworks/citizens/male_09.mdl",
+    "models/willardnetworks/citizens/male_10.mdl",
+}
